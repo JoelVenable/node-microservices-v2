43e45a85ef0328f6e17cbbe417922bf4c7a2703a
Joel Venable <joelmusicman@gmail.com>
2020-07-07 15:58:54 -0500

    Added database deployment and mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { json } from 'body-parser'
-import userRouter from './routes'
-import { errorHandler } from './middlewares'
+import 'express-async-errors';
+import { json } from 'body-parser';
+import userRouter from './routes';
+import { errorHandler } from './middlewares';
 import { NotFoundError } from './@types';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(json());
@@ -10,12 +12,31 @@ app.use(json());
 
 app.use('/api/users', userRouter);
 
-app.all('*', () => { throw new NotFoundError() })
+app.all('*', () => { throw new NotFoundError() });
 
 app.use(errorHandler);
 
 
 
-app.listen(3000, () => {
-    console.log('listening on 3000');
-})
+const start = async () => {
+    try {
+
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Mongodb connected')
+    } catch (err) {
+        console.log('Inside Catch block')
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log('listening on 3000');
+    });
+}
+
+
+
+start()
+
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,8 +1,6 @@
 import { Router, RequestHandler, Request, Response, NextFunction } from "express";
 import { body, validationResult } from 'express-validator'
-import HttpError from "../@types/HttpError";
-
-
+import { RequestValidationError } from "../@types";
 
 const signUp = (userRouter: Router) => userRouter.post('/signup',
     [
@@ -17,12 +15,12 @@ const signUp = (userRouter: Router) => userRouter.post('/signup',
     (req: Request, res: Response, next: NextFunction) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            throw new HttpError(400, 'Invalid email or password')
+            throw new RequestValidationError(errors.array())
         }
 
         const { email, password } = req.body;
 
-        throw new HttpError(500, 'Database connection down')
+
         console.log('Creating user');
         res.status(201).send({ email, password })
     })
