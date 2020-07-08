76b4d19bafb232bb1a1ba8750f6b15dffd2c1f17
Joel Venable <joelmusicman@gmail.com>
2020-07-07 21:01:34 -0500

    Finished sign up flow

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,12 +1,26 @@
-import { prop, getModelForClass, modelOptions, pre } from '@typegoose/typegoose'
+import { prop, getModelForClass, modelOptions, pre, DocumentType } from '@typegoose/typegoose'
 import { Crypto } from '../services'
 
-@modelOptions({ schemaOptions: { collection: 'User', timestamps: true } })
+@modelOptions({
+    schemaOptions: {
+        collection: 'User',
+        timestamps: true,
+        toJSON: {
+            transform: (doc: DocumentType<UserClass>, ret) => {
+                delete ret.password;
+                delete ret.__v;
+                ret.id = ret._id;
+                delete ret._id;
+            }
+        }
+    }
+})
 @pre<UserClass>('save', async function (done) {
     if (this.isModified('password')) {
         const hashed = await Crypto.toHash(this.get('password'))
         this.set('password', hashed)
     }
+    // if (this._id === undefined || this._id === null) this._id = uuid({})
     done()
 })
 class UserClass {
