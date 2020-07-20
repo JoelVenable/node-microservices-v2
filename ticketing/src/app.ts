bfe7e0df7f37560ead48ef5cb5d335f400d4ec70
Joel Venable <joelmusicman@gmail.com>
2020-07-20 18:23:07 -0500

    Updated connection strings

---
+++ ticketing/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { errorHandler, NotFoundError } from '@jdvtickets/common';
+import cookieSession from 'cookie-session'
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+    httpOnly: true,
+}))
+
+
+// app.use('/api/users', userRouter);
+
+app.all('*', () => { throw new NotFoundError() });
+
+app.use(errorHandler);
+
+export default app;
---
+++ ticketing/src/test/setup.ts
@@ -0,0 +1,31 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import app from '../app';
+import { config } from 'dotenv'
+import { join } from 'path'
+
+const path = join(__dirname, '../../../.env.test')
+config({ path })
+let mongo: MongoMemoryServer
+
+beforeAll(async () => {
+    mongo = new MongoMemoryServer();
+    const uri = await mongo.getUri();
+
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+});
+
+
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+    await Promise.all(collections.map(async (c) => c.deleteMany({})));
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+    await mongo.stop();
+});
