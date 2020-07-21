40e341864c4193271ad31a1321789a9f866ba342
Joel Venable <joelmusicman@gmail.com>
2020-07-21 15:17:46 -0500

    Updated common and added validation

--- ticketing/src/routes/newTicket.ts
+++ ticketing/src/routes/newTicket.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { UserRequest, requireAuth } from "@jdvtickets/common";
+import { UserRequest, requireAuth, validateParams, body } from "@jdvtickets/common";
 
 const newTicket = (ticketRouter: Router) => ticketRouter.post('/',
     requireAuth,
+    [
+        body('title').notEmpty().withMessage('Title is required'),
+        body('price').isFloat({ gt: 0 }).notEmpty().withMessage('Price must be greater than 0')
+    ],
+    validateParams,
     async (req: UserRequest, res) => {
         console.log('hello')
         res.sendStatus(200)
