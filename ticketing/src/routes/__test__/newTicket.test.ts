7c489f74e9b868f4a832c56bc88fbd1c243cff0e
Joel Venable <joelmusicman@gmail.com>
2020-07-21 15:04:16 -0500

    Another failing test

--- ticketing/src/routes/__test__/newTicket.test.ts
+++ ticketing/src/routes/__test__/newTicket.test.ts
@@ -7,17 +7,22 @@ const user = {
     password: 'passw0rd!'
 }
 
+const postRoute = () => request(app).post('/api/tickets')
+
+const postSignedIn = () => {
+    const { session } = signin(user)
+    return postRoute().set('Cookie', session)
+}
+
 it('has a route handler listening to POST /api/tickets', async () => {
-    const response = await request(app)
-        .post('/api/tickets')
+    const response = await postRoute()
         .send({});
     expect(response.status).not.toEqual(404)
 })
 
 
 it('can only be accessed if user is signed in', async () => {
-    const response = await request(app)
-        .post('/api/tickets')
+    const response = await postRoute()
         .send({});
 
     const errorResponse = {
@@ -34,15 +39,18 @@ it('can only be accessed if user is signed in', async () => {
 it('returns a status other than 401 if user is signed in', async () => {
 
     const { session } = signin(user)
-    const response = await request(app)
-        .post('/api/tickets')
-        .set('Cookie', session)
+    const response = await postSignedIn()
         .send({});
     expect(response.status).not.toEqual(401)
 })
 
-it('returns an error if invalid title is provided', () => {
-
+it('returns an error if invalid title is provided', async () => {
+    const response = await postSignedIn()
+        .send({
+            title: '',
+            price: 10
+        })
+    expect(response.status).toEqual(400)
 })
 
 it('returns an error if invalid price is provided', () => {
